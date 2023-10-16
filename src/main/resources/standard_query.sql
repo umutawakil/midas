f32bb1de1e0c7f2c884fa160904e9f34f93b651f
Usman Mutawakil <umutawakil@Usmans-MacBook-Pro.local>
2023-10-16 02:13:58 -0400

    Improved queries

---
+++ src/main/resources/standard_query.sql
@@ -0,0 +1,14 @@
+/** This query should show the lowest risk stocks. Sort by running_delta desc during trading hours
+To find whose moving. You can find higher delta stocks but very little will suggest you will have time to
+jump in on those. Tweek this query to try riskier jumps if thats desired.
+ **/
+
+SELECT * FROM midas.v_milestone 
+WHERE
+	max_delta <= 20 AND
+    min_delta >= -15 AND
+    time_window = 60 AND
+    max_price <= 300 AND
+    price >= 0.1 AND
+    window_delta >= 20
+ORDER BY running_delta DESC,window_delta DESC; /** Need to play with what your sorting on **/
